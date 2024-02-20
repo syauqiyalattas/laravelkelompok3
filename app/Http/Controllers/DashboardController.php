2e1ae21107cf3c2921d0c9de8cacc6c52233851e
yericoalexander <yericoalexander12@gmail.com>
2024-02-20 10:50:45 +0700

    terbaru

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,6 +8,6 @@ class DashboardController extends Controller
 {
     public function index() 
     {
-        return view('pages.dasboard');
+        return view('pages.dashboard');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,4 +18,4 @@ Route::get('/', function (){
     return view('welcome');
 });
 
-Route::get('/dasboard', [DasboardController::class,'index']);
+Route::get('/dashboard', [DashboardController::class,'index']);
