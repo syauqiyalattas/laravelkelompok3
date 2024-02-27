c9f92bcbebd6ff54080f0b7df2e7d377f48ad0b5
alexantasy <zeraze30@mail.com>
2024-02-27 11:00:47 +0700

    ini nambahin migrattion tpi blm selese

---
+++ database/migrations/2024_02_27_031244_create_profile_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->id();
+            $table->srting();
+            $table->integer('no_hp');
+            $table->srting();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profile');
+    }
+};
