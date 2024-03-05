cc21c6f223e3cf689701c0e0833aa3c35b023304
yericoalexander <yericoalexander12@gmail.com>
2024-03-05 09:48:57 +0700

    hyyy

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PelangganController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,4 @@ Route::get('/', function (){
 });
 
 Route::get('/dashboard', [DashboardController::class,'index']);
+Route::get('/pelanggan', [PelangganController::class,'index']);
